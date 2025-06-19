6298eb2536541f331c0f8cb611d7c40631df6eb6
arun2004-ar <arunprashanth2102004@gmail.com>
2025-06-19 22:04:10 +0530

    Add files via upload

---
+++ app10.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def app():
+    st.title("Indian Engineers Elite Challenge 2025 - Thanksgiving Video")
+    
+    st.write("## Watch the Official Thanksgiving Video")
+    st.video("https://raw.githubusercontent.com/Keerthivasan-11/ISA/main/WhatsApp%20Video%202025-03-22%20at%2018.58.57.mp4")
+    
+if __name__ == "__main__":
+    app()
+
